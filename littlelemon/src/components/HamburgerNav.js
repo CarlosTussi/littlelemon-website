f0b4726298545b839a283678c604aca98b972f70
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-01-07 18:11:04 +0100

    [ADD] SideMenu implementation

--- littlelemon/src/components/HamburgerNav.js
+++ littlelemon/src/components/HamburgerNav.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import hamburgerIcon from '../assets/hamburger.svg'
 import NavItems from '../NavItems';
 import {X,} from 'react-feather';
@@ -7,26 +7,44 @@ function HamburgerNav()
 {
 
     const [isOpen, setIsOpen] = useState(false);
+    const sideMenuRef = useRef();
+
+     useEffect(()=>{
+        if(sideMenuRef.current) //Avoid first render
+            if(isOpen)
+            {                
+                sideMenuRef.current.style.transform = "translateX(0em)";  
+            }
+            else{                
+                sideMenuRef.current.style.transform = "translateX(-20em)";
+            }
+    },[isOpen])
+
     return(
         <nav className="hamburgerNav">            
-            {isOpen ? 
+
                         <>
                             
-                            <div className="transparentBackground" aria-hidden="true"/>
-                            <div className="openHamburgerMenu">
-                                <X size="2em" onClick={() => setIsOpen(false)}/>
+                            {isOpen? <div className="transparentBackground" aria-hidden="true"/>: ""}
+                            <div className="openHamburgerMenu"
+                                 ref={sideMenuRef}
+                                style={
+                                    isOpen ? {right: "0em"} : {right: "-40em"}
+                                }
+                                 >
+                                <X size="2em" onClick={() => setIsOpen(false) }/>
                                 <NavItems/> 
                             </div>                            
                             
                         </>                           
-                        :
+
                         <img src={hamburgerIcon} 
                              role="menu button" 
                              alt="Three lines of equal size stacked"
                              onClick={() => setIsOpen(true)}/>
                     
                         
-            }
+            
             
             
             
@@ -35,5 +53,3 @@ function HamburgerNav()
 }
 
 export default HamburgerNav;
-
- {/* <div className="transparentBackground" aria-hidden="true"/>  */}
