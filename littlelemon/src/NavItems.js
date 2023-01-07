3699423e61d2f6595b62c5a955f04ec8acfa1730
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-01-07 16:52:01 +0100

    [ADD] Colapsable menu without smooth transition

--- littlelemon/src/NavItems.js
+++ littlelemon/src/NavItems.js
@@ -4,15 +4,13 @@ function NavItems()
 {
     return(
         <>
-            <ul>
-                <ul>
-                    <li><Link to="/" className='navLink'>Home</Link></li>
-                    <li><Link to="/comingsoon" className='navLink'>About</Link></li>
-                    <li><Link to="/comingsoon" className='navLink'>Menu</Link></li>
-                    <li><Link to="/reservations" className='navLink'>Reservations</Link></li>
-                    <li><Link to="/comingsoon" className='navLink'>Order Online</Link></li>
-                    <li><Link to="/comingsoon" className='navLink'>Login</Link></li>                    
-                </ul>
+             <ul>
+                <li><Link to="/" className='navLink'>Home</Link></li>
+                <li><Link to="/comingsoon" className='navLink'>About</Link></li>
+                <li><Link to="/comingsoon" className='navLink'>Menu</Link></li>
+                <li><Link to="/reservations" className='navLink'>Reservations</Link></li>
+                <li><Link to="/comingsoon" className='navLink'>Order Online</Link></li>
+                <li><Link to="/comingsoon" className='navLink'>Login</Link></li>                    
             </ul>
         </>
     )
--- littlelemon/src/components/HamburgerNav.js
+++ littlelemon/src/components/HamburgerNav.js
@@ -1,12 +1,39 @@
+import { useState } from 'react';
 import hamburgerIcon from '../assets/hamburger.svg'
+import NavItems from '../NavItems';
+import {X,} from 'react-feather';
 
 function HamburgerNav()
 {
+
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <nav className="hamburgerNav">
-            <img src={hamburgerIcon} alt="Three lines of equal size stacked"/>
+        <nav className="hamburgerNav">            
+            {isOpen ? 
+                        <>
+                            
+                            <div className="transparentBackground" aria-hidden="true"/>
+                            <div className="openHamburgerMenu">
+                                <X size="2em" onClick={() => setIsOpen(false)}/>
+                                <NavItems/> 
+                            </div>                            
+                            
+                        </>                           
+                        :
+                        <img src={hamburgerIcon} 
+                             role="menu button" 
+                             alt="Three lines of equal size stacked"
+                             onClick={() => setIsOpen(true)}/>
+                    
+                        
+            }
+            
+            
+            
         </nav>
     )
 }
 
 export default HamburgerNav;
+
+ {/* <div className="transparentBackground" aria-hidden="true"/>  */}
